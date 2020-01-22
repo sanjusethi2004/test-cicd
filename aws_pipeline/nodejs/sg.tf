73c2727ef2a329c042cf13a6d85b2adcfb4cce02
sanjayakumar sethi <sanjayakumar.sethi@morningstar.com>
2020-01-22 13:25:02 +0530

    updated webec2.json

--- aws_pipeline/nodejs/sg.tf
+++ aws_pipeline/nodejs/sg.tf
@@ -15,6 +15,12 @@ resource "aws_security_group" "web-instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "web-instance"
   }
